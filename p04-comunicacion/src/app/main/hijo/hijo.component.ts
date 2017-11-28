79db0eb2b6e1b17f463214d03328a68f1768e83d
filipmorrys <fdiazro@gmail.com>
2017-11-28 18:08:20 +0100

    comunicacion entre moduloss

---
+++ p04-comunicacion/src/app/main/hijo/hijo.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-hijo',
+  templateUrl: './hijo.component.html',
+  styleUrls: ['./hijo.component.css']
+})
+export class HijoComponent implements OnInit {
+
+  @Input() sNombre: string;
+  @Output() eRespuesta: EventEmitter<string>;
+  sNuevoNombre: string;
+
+  constructor() {
+    // Hay que instanciarlo en el constructor puesto que ngOnInit se ejecuta
+    // demasiado tarde
+    this.eRespuesta = new EventEmitter();
+  }
+
+  ngOnInit() {
+    this.sNuevoNombre = '';
+  }
+
+  responder() {
+    this.eRespuesta.emit(this.sNuevoNombre);
+  }
+
+}
