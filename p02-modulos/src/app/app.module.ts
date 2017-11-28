456e70807e08deb3a10ef91da30e4ff0fe6d616b
filipmorrys <fdiazro@gmail.com>
2017-11-28 13:03:05 +0100

    p02-modulos nueva applicacion angular

--- p02-modulos/src/app/app.module.ts
+++ p02-modulos/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { CabezaComponent } from './cabeza/cabeza.component';
+import { SharedModule } from './shared/shared.module';
+import { MainModule } from './main/main.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MainModule,
+    SharedModule
   ],
   providers: [],
-  bootstrap: [AppComponent] // bootstrap solo en el componente principal, en el resto exports
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
